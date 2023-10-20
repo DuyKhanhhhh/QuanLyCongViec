894a0589648f4750a644ac7a0c0b8f932003281d
duonghongduc2k4 <duc2022004@gmail.com>
2023-10-20 13:24:21 +0700

    loginAndSignUp

--- src/main/java/com/example/projectqlcv/controller/UpdatePasswordController.java
+++ src/main/java/com/example/projectqlcv/controller/UpdatePasswordController.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+
 @WebServlet(name = "UpdatePasswordController", value = "/updatePassword")
 public class UpdatePasswordController extends HttpServlet {
     UserDAO userDAO;
@@ -41,17 +42,15 @@ public class UpdatePasswordController extends HttpServlet {
             if (loginUser != null) {
                 if (newPassword.equals(confirmPassword) && !password.equals(newPassword)) {
                     userDAO.editPassWordUser(email, newPassword);
-                    request.setAttribute("message","Update success !");
-                    request.getRequestDispatcher("login.jsp").forward(request,response);
+                    request.setAttribute("message", "Update success !");
+                    request.getRequestDispatcher("login.jsp").forward(request, response);
                 } else {
                     request.setAttribute("message", "Erorr Password Not Synchronized !");
-                    request.getRequestDispatcher("editPassword.jsp").forward(request,response);
-
+                    request.getRequestDispatcher("editPassword.jsp").forward(request, response);
                 }
             } else {
                 request.setAttribute("message", "Wrong email or password !");
-                request.getRequestDispatcher("editPassword.jsp").forward(request,response);
-
+                request.getRequestDispatcher("editPassword.jsp").forward(request, response);
             }
         } catch (ServletException e) {
             throw new RuntimeException(e);
